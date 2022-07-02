c02d9c62c7b85f2597a775e18d7b47b5470b17b1
neootavio <99883591+neootavio@users.noreply.github.com>
2022-07-02 10:37:48 -0300

    iniciais

---
+++ exercicios/Exercicio2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exercicio2
+{
+
+	
+
+	public static void main(String args[]){
+
+		Scanner input = new Scanner(System.in);
+
+		int c, thisIsAVariable, q76354, number, value;
+
+		System.out.print("Digite um número: ");
+		value = input.nextInt();
+
+
+		System.out.println("This is a Java program.");
+
+		System.out.println("This is a Java");
+
+		System.out.println("program.");
+
+		System.out.printf("%s\n%s\n", "This is a Java","program.");
+
+
+
+
+
+	}
+}
---
+++ exercicios/Welcome1.java
@@ -0,0 +1,9 @@
+public class Welcome1
+{
+
+	public static void main(String args[]){
+
+	System.out.println("Hello Java!");
+
+	}
+}
