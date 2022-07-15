9ca7cb894eb11fb47b0de83f7790a9b8e68704e8
neootavio <99883591+neootavio@users.noreply.github.com>
2022-07-15 08:07:46 -0300

    Create Exercicio28.java

---
+++ exercicios/Exercicio28.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio28
+{
+
+    Scanner input = new Scanner(System.in);
+
+    int number;
+
+    int a, b, c;
+
+    System.out.print("Enter an integer: ");
+    number = input.nextInt();
+
+    a = b * c;
+
+
+
+    /**Este programa calcula uma folha de pagamento. */
+
+}
